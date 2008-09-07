8b1933f06932d688b97ef4e52e1331f05e4ea417
Tekkub <tekkub@gmail.com>
2008-09-06 23:51:22 -0600

    Remove unused code

--- tooltips.lua
+++ tooltips.lua
@@ -15,16 +15,6 @@ local OnTooltipSetItem = function(frame, ...)
 		if min then frame:AddDoubleLine("AH buyout:", max and max ~= min and (tekauc:GS(min).." - "..tekauc:GS(max)) or tekauc:GS(min)) end
 		if min and (maxStack or 0) > 1 then frame:AddDoubleLine("AH stack buyout:", max and max ~= min and (tekauc:GS(min*maxStack).." - "..tekauc:GS(max*maxStack)) or tekauc:GS(min*maxStack)) end
 		if count then frame:AddDoubleLine("Number on AH:", count) end
-
---~ 		if val and (maxStack or 0) > 1 then
---~ 			frame:AddDoubleLine("Lowest AH buyout:", tekauc:GS(val).." ("..tekauc:GS(val*maxStack)..")")
---~ 			frame:AddDoubleLine("Highest AH buyout:", tekauc:GS(tekauc.maxes[id]).." ("..tekauc:GS(tekauc.maxes[id]*maxStack)..")")
-
---~ 		elseif val then
---~ 			frame:AddDoubleLine("Lowest AH buyout:", tekauc:GS(val))
---~ 			frame:AddDoubleLine("Highest AH buyout:", tekauc:GS(tekauc.maxes[id]))
---~ 			frame:AddDoubleLine("Number on AH:", tekauc.counts[id])
---~ 		end
 	end
 	if origs[frame] then return origs[frame](frame, ...) end
 end
