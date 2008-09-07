65ad5273f24ef2406252c999f36ff0352d84efe7
Tekkub <tekkub@gmail.com>
2008-09-06 22:19:59 -0600

    Remove partial scanner, make button trigger all-scan

--- scanner.lua
+++ scanner.lua
@@ -1,15 +1,26 @@
 
---~ CanSendAuctionQuery("list")
-
 local ids = LibStub("tekIDmemo")
-local page, scanning, lastscanned, butt
 local mins, maxes, counts, lastseen = {}, {}, {}, {}
-local name, minLevel, maxLevel, invTypeIndex, classIndex, subclassIndex, _, isUsable, qualityIndex
 
 tekauc.mins, tekauc.maxes, tekauc.counts = mins, maxes, counts
 
 
-local function scanpage()
+BrowseSearchButton:SetPoint("LEFT", IsUsableCheckButton, "RIGHT", 10, -8)
+local butt = LibStub("tekKonfig-Button").new(BrowseSearchButton, "BOTTOM", BrowseSearchButton, "TOP")
+butt:SetText("Scan All")
+
+butt:SetScript("OnClick", function()
+	if not select(2, CanSendAuctionQuery("list")) then return ChatFrame1:AddMessage("Cannot scan yet") end
+
+	ChatFrame1:AddMessage("Sending all-scan query")
+	mins, maxes, counts = {}, {}, {}
+	tekauc.mins, tekauc.maxes, tekauc.counts = mins, maxes, counts
+	QueryAuctionItems(nil, nil, nil, nil, nil, nil, nil, nil, nil, true)
+	self:RegisterEvent("AUCTION_ITEM_LIST_UPDATE")
+end)
+
+butt:SetScript("OnEvent", function(self)
+	self:UnregisterEvent("AUCTION_ITEM_LIST_UPDATE")
 	for i=1,GetNumAuctionItems("list") do
 		local name, texture, count, quality, canUse, level, minBid, minIncrement, buyoutPrice, bidAmount, highBidder, owner = GetAuctionItemInfo("list", i)
 		local id = ids[GetAuctionItemLink("list", i)]
@@ -20,75 +31,8 @@ local function scanpage()
 			counts[id] = (counts[id] or 0) + count
 		end
 	end
-	return GetNumAuctionItems("list")
-end
 
-
-local allscanframe = CreateFrame("Frame", nil, AuctionFrame)
-allscanframe:SetScript("OnShow", function(self)
-	if not select(2, CanSendAuctionQuery("list")) then return end
-
-	ChatFrame1:AddMessage("Sending all-scan query")
-	mins, maxes, counts = {}, {}, {}
-	tekauc.mins, tekauc.maxes, tekauc.counts = mins, maxes, counts
-	QueryAuctionItems(nil, nil, nil, nil, nil, nil, nil, nil, nil, true)
-	self:RegisterEvent("AUCTION_ITEM_LIST_UPDATE")
-end)
-
-
-allscanframe:SetScript("OnEvent", function(self)
-	self:UnregisterEvent("AUCTION_ITEM_LIST_UPDATE")
-	ChatFrame1:AddMessage("Time to scan")
-	local count = scanpage()
-	ChatFrame1:AddMessage("Done scanning "..count.. " items")
-end)
-
-
-local scanframe = CreateFrame("Frame")
-scanframe:Hide()
-
-
-function tekauc:Scan(...)
-	name, minLevel, maxLevel, invTypeIndex, classIndex, subclassIndex, _, isUsable, qualityIndex = ...
-	page, scanning = -1, GetTime()
-	scanframe:Show()
-	butt:Disable()
-end
-
-
-scanframe:SetScript("OnUpdate", function(self)
-	if not CanSendAuctionQuery("list") then return end
-	self:Hide()
-	self:RegisterEvent("AUCTION_ITEM_LIST_UPDATE")
-	page = page + 1
-	QueryAuctionItems(name, minLevel, maxLevel, invTypeIndex, classIndex, subclassIndex, page, isUsable, qualityIndex)
-end)
-
-
-scanframe:SetScript("OnEvent", function(self)
-	if not scanning or page == lastscanned then return end
-
-	scanpage()
-	lastscanned = page
-	local numbatch, numtotal = GetNumAuctionItems("list")
-
-	if NUM_AUCTION_ITEMS_PER_PAGE * page + numbatch < numtotal then self:Show()
-	else
-		local time = GetTime() - scanning
-		scanning = nil
-		self:UnregisterEvent("AUCTION_ITEM_LIST_UPDATE")
-		butt:Enable()
-		tekauc:Print(time < 60 and string.format("Scan took %d seconds", time) or string.format("Scan took %.1f minutes", time/60))
-	end
-end)
-
-
-BrowseSearchButton:SetPoint("LEFT", IsUsableCheckButton, "RIGHT", 10, -8)
-butt = LibStub("tekKonfig-Button").new(BrowseSearchButton, "BOTTOM", BrowseSearchButton, "TOP")
-butt:SetText("Scan")
-butt:SetScript("OnClick", function()
-	tekauc:Scan(BrowseName:GetText(), BrowseMinLevel:GetText(), BrowseMaxLevel:GetText(), AuctionFrameBrowse.selectedInvtypeIndex, AuctionFrameBrowse.selectedClassIndex,
-		AuctionFrameBrowse.selectedSubclassIndex, AuctionFrameBrowse.page, IsUsableCheckButton:GetChecked(), UIDropDownMenu_GetSelectedValue(BrowseDropDown))
+	ChatFrame1:AddMessage("Done scanning ".. GetNumAuctionItems("list").. " items")
 end)
 
 
