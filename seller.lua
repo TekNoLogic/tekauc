37815d05209e1ac254c70d508ccbd5f497e65334
Tekkub <tekkub@gmail.com>
2008-12-02 14:48:44 -0700

    Add mass-split and mass-sel for gems and glyphs

--- seller.lua
+++ seller.lua
@@ -79,6 +79,13 @@ f:SetScript("OnEvent", function(self, event, bag)
 end)
 
 
+local function GetPrice(link, stack)
+	local price = link and GetAuctionBuyout(link)
+	if not price then return end
+	return math.floor((price*stack - 1)/500) * 500 -- Rounds down to the next multiple of 5s
+end
+
+
 local orig = ContainerFrameItemButton_OnModifiedClick
 ContainerFrameItemButton_OnModifiedClick = function(self, button, ...)
 	if AuctionFrame:IsShown() and IsAltKeyDown() then
@@ -89,16 +96,74 @@ ContainerFrameItemButton_OnModifiedClick = function(self, button, ...)
 			SlashCmdList.TEKSPLITTER(link.." 1")
 		else
 			local _, stack = GetContainerItemInfo(bag, slot)
-			local price = link and GetAuctionBuyout(link)
+			local price = GetPrice(link, stack)
 			if not price then
 				if link then Print("Cannot find price for", link) else Print("Error finding item") end
 				return orig(button, ...)
 			end
 
-			price = math.floor((price*stack - 1)/500) * 500 -- Rounds down to the next multiple of 5s
 			Print("Queueing ", link, "for sale at ", price)
 			tekauc:PostBatch(ids[link], price, stack)
 		end
 	else return orig(self, button, ...) end
 end
+
+
+local blist = [[
+  818   774  1206  1210  1529  1705
+ 3864  7909  7910 12361 12364 12799 12800
+23077 21929 23112 23079 23117 23107
+23436 23439 23440 23437 23438 23441
+32227 32231 32229 32249 32228 32230
+36917 36929 36920 36932 36923 36926
+36918 36930 36921 36933 36924 36927
+ 5498  5500  7971 13926 24478 36783 24479
+25867 25868 36784 41266 41334 42225
+11382 12363 19774
+]]
+local butt = LibStub("tekKonfig-Button").new(AuctionFrameAuctions, "TOPRIGHT", 20, -13)
+butt:SetText("Sell Gems")
+butt.tiptext = "Post all cut gems in your bags"
+butt:SetScript("OnClick", function(self)
+	for bag=0,4 do
+		for slot=1,GetContainerNumSlots(bag) do
+			local link = GetContainerItemLink(bag, slot)
+			if link and select(6, GetItemInfo(link)) == "Gem" and not blist:match(ids[link]) and select(2, GetContainerItemInfo(bag, slot)) == 1 then
+				local price = GetPrice(link, 1)
+				if price then tekauc:PostBatch(ids[link], price, 1) end
+			end
+		end
+	end
+end)
+
+
+local butt2 = LibStub("tekKonfig-Button").new(butt, "RIGHT", butt, "LEFT", -20, 0)
+butt2:SetText("Sell Glyphs")
+butt2.tiptext = "Post all single-stack glyphs in your bags"
+butt2:SetScript("OnClick", function(self)
+	for bag=0,4 do
+		for slot=1,GetContainerNumSlots(bag) do
+			local link = GetContainerItemLink(bag, slot)
+			if link and select(6, GetItemInfo(link)) == "Glyph" and select(2, GetContainerItemInfo(bag, slot)) == 1 then
+				local price = GetPrice(link, 1)
+				if price then tekauc:PostBatch(ids[link], price, 1) end
+			end
+		end
+	end
+end)
+
+
+local butt3 = LibStub("tekKonfig-Button").new(butt, "RIGHT", butt2, "LEFT")
+butt3:SetText("Split Glyphs")
+butt3.tiptext = "Split glyph stacks into singles"
+butt3:SetScript("OnClick", function(self)
+	for bag=0,4 do
+		for slot=1,GetContainerNumSlots(bag) do
+			local link = GetContainerItemLink(bag, slot)
+			if link and select(6, GetItemInfo(link)) == "Glyph" and select(2, GetContainerItemInfo(bag, slot)) ~= 1 then
+				SlashCmdList.TEKSPLITTER(link.." 1")
+			end
+		end
+	end
+end)
 
