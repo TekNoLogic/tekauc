aa60542af36ac9654646836b893518ff53322e37
tekkub :bear:  <tekkub@gmail.com>
2012-09-18 05:59:45 -0600

    Allow for erasing manual prices by passing 0 price

--- manualprices.lua
+++ manualprices.lua
@@ -6,7 +6,12 @@ SLASH_TEKAUCPRICE1 = "/taprice"
 SlashCmdList.TEKAUCPRICE = function(input)
 	local id, price = string.match(input, "item:(%d+):.*%|h%|r%s*([%d]+)%s*$")
 	if not id or not price then return Print("Usage: /taprice [Item Link] price") end
-	tekauc.manualprices[tonumber(id)] = tonumber(price)
+	local price = tonumber(price)
+	if not price or price == 0 then
+		tekauc.manualprices[tonumber(id)] = nil
+	else
+		tekauc.manualprices[tonumber(id)] = tonumber(price)
+	end
 	Print("Set manual price", GetItemInfo(id), "to", price)
 end
 
