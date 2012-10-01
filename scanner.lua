47cdd53b7f5b2592177bed754e8cf0f52c56c1d2
tekkub :bear:  <tekkub@gmail.com>
2012-10-01 03:27:44 -0600

    Fix scanner

--- scanner.lua
+++ scanner.lua
@@ -42,9 +42,8 @@ butt:SetScript("OnEvent", function(self)
 
 	for i=1,num do
 		local name, texture, count, quality, canUse, level, levelColHeader, minBid,
-			minIncrement, buyoutPrice, bidAmount, highBidder, owner, saleStatus,
-			itemId, hasAllInfo = GetAuctionItemInfo("list", i)
-		local id = ids[GetAuctionItemLink("list", i)]
+			minIncrement, buyoutPrice, bidAmount, highBidder, owner, saleStatus, id,
+			hasAllInfo = GetAuctionItemInfo("list", i)
 
 		if not allscan and not touched[id] then touched[id], mins[id], maxes[id], counts[id] = true end -- Wipe these results if it's a short scan
 		buyoutPrice = buyoutPrice / count
