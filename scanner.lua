527d073a7ba3c1f77be70ed1089d1a73167f5f1c
tekkub :bear:  <tekkub@gmail.com>
2014-10-22 02:21:15 -0600

    Throttle the scan so we don't CTD

--- scanner.lua
+++ scanner.lua
@@ -2,7 +2,8 @@
 local myname, ns = ...
 
 local INFLATION_LIMIT = 2.5 -- Maximum markup we'll allow over manually set prices
-local mins, maxes, counts, lastseen, allscan = {}, {}, {}, {}
+local mins, maxes, counts, lastseen = {}, {}, {}, {}
+local allscaninprogress, touched
 
 tekauc.mins, tekauc.maxes, tekauc.counts = mins, maxes, counts
 
@@ -10,8 +11,58 @@ tekauc.mins, tekauc.maxes, tekauc.counts = mins, maxes, counts
 local butt = LibStub("tekKonfig-Button").new(BrowseSearchButton, "RIGHT", BrowseSearchButton, "LEFT", -10, 0)
 butt:SetText("Scan All")
 
+
+local function IsValidPrice(id, price)
+	if not tekauc.manualprices[id] then return true end
+	if (tekauc.manualprices[id] * INFLATION_LIMIT) >= price then return true end
+end
+
+
+local function ScanBlock(startindex, endindex)
+	for i=startindex,endindex do
+		local name, texture, count, quality, canUse, level, levelColHeader, minBid,
+			minIncrement, buyoutPrice, bidAmount, highBidder, bidderFullName, owner,
+			ownerFullName, saleStatus, id, hasAllInfo = GetAuctionItemInfo("list", i)
+
+		if not allscaninprogress and not touched[id] then
+			-- Wipe these results if it's a short scan
+			touched[id], mins[id], maxes[id], counts[id] = true
+		end
+		buyoutPrice = buyoutPrice / count
+		if buyoutPrice > 0 and IsValidPrice(id, buyoutPrice) then
+			if (mins[id] or 9999999999) > buyoutPrice then mins[id] = buyoutPrice end
+			if (maxes[id] or 0) < buyoutPrice then maxes[id] = buyoutPrice end
+			counts[id] = (counts[id] or 0) + count
+		end
+	end
+end
+
+
 local enabled = true
+local BLOCKSIZE = 1000 - 1
+local TICKLENGTH = 0.15
+local totalresults, nextblock, starttime, nexttick
 butt:SetScript("OnUpdate", function(self)
+	-- First check if we need to process a block of results
+	if allscaninprogress and nexttick <= GetTime() then
+		local endindex = nextblock + BLOCKSIZE
+		if endindex > totalresults then endindex = totalresults end
+
+		ScanBlock(nextblock, endindex)
+
+		if endindex == totalresults then
+			for _,sellbutt in pairs(ns.sellbutts) do sellbutt:Enable() end
+			local elap = GetTime() - starttime
+			ns.Print("Done scanning: ", totalresults, "items in", elap, "seconds")
+
+			allscaninprogress = false
+			totalresults, nextblock, starttime = nil
+		else
+			nextblock = endindex + 1
+			nexttick = GetTime() + TICKLENGTH
+		end
+	end
+
 	local _, scanable = CanSendAuctionQuery("list")
 	if enabled and not scanable then self:Disable()
 	elseif not enabled and scanable then
@@ -21,11 +72,13 @@ butt:SetScript("OnUpdate", function(self)
 	enabled = scanable
 end)
 
+local allscanpending, querytime
 butt:SetScript("OnClick", function(self)
-	ns.Print("Sending all-scan query", GetTime())
+	querytime = GetTime()
+	ns.Print("Sending all-scan query")
 	mins, maxes, counts = {}, {}, {}
 	tekauc.mins, tekauc.maxes, tekauc.counts = mins, maxes, counts
-	allscan = true
+	allscanpending = true
 	QueryAuctionItems(nil, nil, nil, nil, nil, nil, nil, nil, nil, true)
 end)
 
@@ -34,32 +87,19 @@ butt:SetScript("OnEvent", function(self)
 	local num, total = GetNumAuctionItems("list")
 	if total < 10000000 and num ~= total then return end
 
-	local starttime = GetTime()
-	if allscan then ns.Print("Starting scan") end
 
-	local touched = {}
-
-	for i=1,num do
-		local name, texture, count, quality, canUse, level, levelColHeader, minBid,
-			minIncrement, buyoutPrice, bidAmount, highBidder, bidderFullName, owner,
-			ownerFullName, saleStatus, id, hasAllInfo = GetAuctionItemInfo("list", i)
-
-		if not allscan and not touched[id] then touched[id], mins[id], maxes[id], counts[id] = true end -- Wipe these results if it's a short scan
-		buyoutPrice = buyoutPrice / count
-		if buyoutPrice > 0 and (not tekauc.manualprices[id] or (tekauc.manualprices[id] * INFLATION_LIMIT) >= buyoutPrice) then
-			if (mins[id] or 9999999999) > buyoutPrice then mins[id] = buyoutPrice end
-			if (maxes[id] or 0) < buyoutPrice then maxes[id] = buyoutPrice end
-			counts[id] = (counts[id] or 0) + count
-		end
+	if allscanpending then
+		allscanpending = false
+	 	allscaninprogress = true
+		starttime = GetTime()
+		touched, totalresults, nextblock = {}, num, 1
+		nexttick = starttime
+		ns.Print("Server response", starttime - querytime, "seconds")
+		ns.Print("Starting scan")
+	elseif not allscaninprogress and num < 5000 then
+		touched, totalresults, nextblock = {}, num, 1
+		ScanBlock(1, num)
 	end
-
-	for _,sellbutt in pairs(ns.sellbutts) do sellbutt:Enable() end
-	if allscan then
-		local elap = GetTime() - starttime
-		ns.Print("Done scanning: ", num, "items in", elap, "seconds")
-	end
-
-	allscan = false
 end)
 
 
