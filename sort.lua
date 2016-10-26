58f2e76ab52c8cacabd2e580421e661a0cdd4e7c
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-26 04:00:47 -0600

    re-apply default sort after a scan

--- sort.lua
+++ sort.lua
@@ -1,3 +1,11 @@
 
+local myname, ns = ...
+
+
 -- Default to sort by cheapest unit price
 SortAuctionSetSort("list", "unitprice")
+
+-- Restore after a scan
+ns.RegisterCallback({}, "SCAN_COMPLETE", function()
+  SortAuctionSetSort("list", "unitprice")
+end)
